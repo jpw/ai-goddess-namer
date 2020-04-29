3cc329f7882752b3695787557000edf96ed06e9d
jpw <jon@mdi.eu.com>
2020-04-29 17:49:15 +0100

    mkdir weights, adds comments

--- train.py
+++ train.py
@@ -2,10 +2,21 @@
 
 from textgenrnn import textgenrnn
 t = textgenrnn()
-t.train_from_file('inputs/goddess-name-wordlist.txt', num_epochs=5)
-
 
 """
+t = textgenrnn()
+is fine the first time you run the script, but if you’d like to come back to it later, enter the name 
+of the .hdf5 file generated. 
+t=textgenrnn('weights/textgenrnn_weights.hdf5')
+"""
+
+t.train_from_file('inputs/goddess-name-wordlist.txt', num_epochs=5)
+"""
+num_epochs is how many times you’d like to process the file. The neural network gets better the longer you let it study, so start with 2 or 5 to see how long that takes, and then go up from there.
+"""
+
+"""
+wordlist source:
 https://en.wikipedia.org/wiki/List_of_goddesses
 let l = [];Array.from(document.querySelectorAll('li')).filter(el => !/\d/.test(el.innerText)).forEach(i=> l.push(i.innerText));console.log(l)
 """
